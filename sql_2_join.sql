f027fd61e60ad548ed190884017e24d3a0059d38
Daniel Wennström <daniel@wennstrom.io>
2025-03-12 11:09:28 +0100

    updated w/ queries

--- sql_2_join.sql
+++ sql_2_join.sql
@@ -38,76 +38,74 @@ SELECT CountryCode FROM world.city GROUP BY CountryCode HAVING COUNT(*) > 200;
 SELECT CountryCode FROM world.city GROUP BY CountryCode HAVING COUNT(*) > 200 ORDER BY COUNT(*);
 #
 # 13: What language(s) is spoken in the city with a population between 400 and 500 ?
-Italian
+SELECT cl.Language
+FROM world.city ci
+INNER JOIN world.countrylanguage  cl
+ON ci.CountryCode = cl.CountryCode
+WHERE ci.Population BETWEEN 400 AND 500;
 #
 # 14: What are the name(s) of the cities with a population between 500 and 600 people and the language(s) spoken in them
-'The Valley', 'English'
-'Bantam', 'English'
-'Bantam', 'Malay'
-'Yaren', 'Chinese'
-'Yaren', 'English'
-'Yaren', 'Kiribati'
-'Yaren', 'Nauru'
-'Yaren', 'Tuvalu'
+SELECT ci.Name, cl.Language
+FROM world.city ci
+INNER JOIN world.countrylanguage cl
+ON ci.CountryCode = cl.CountryCode
+WHERE ci.Population BETWEEN 500 AND 600;
 #
 # 15: What names of the cities are in the same country as the city with a population of 122199 (including the that city itself)
-'Stockholm'
-'Gothenburg [Göteborg]'
-'Malmö'
-'Uppsala'
-'Linköping'
-'Västerås'
-'Örebro'
-'Norrköping'
-'Helsingborg'
-'Jönköping'
-'Umeå'
-'Lund'
-'Borås'
-'Sundsvall'
-'Gävle'
+SELECT ci.Name
+FROM world.city ci
+WHERE ci.CountryCode = (
+    SELECT c.CountryCode
+    FROM world.city c
+    WHERE c.Population = 122199
+)
 #
 # 16: What names of the cities are in the same country as the city with a population of 122199 (excluding the that city itself)
-'Stockholm'
-'Gothenburg [Göteborg]'
-'Malmö'
-'Uppsala'
-'Linköping'
-'Västerås'
-'Örebro'
-'Helsingborg'
-'Jönköping'
-'Umeå'
-'Lund'
-'Borås'
-'Sundsvall'
-'Gävle'
+SELECT ci.Name
+FROM world.city ci
+WHERE ci.CountryCode = (
+    SELECT c.CountryCode
+    FROM world.city c
+    WHERE c.Population = 122199
+)
+AND ci.Population != 122199;
 #
 # 17: What are the city names in the country where Luanda is capital?
-'Luanda'
-'Huambo'
-'Lobito'
-'Benguela'
-'Namibe'
+SELECT ci.Name
+FROM world.city ci
+JOIN world.country c ON ci.CountryCode = c.Code
+WHERE c.Capital = (
+    SELECT ci2.ID
+    FROM world.city ci2
+    WHERE ci2.Name = 'Luanda'
+);
+
 #
 # 18: What are the names of the capital cities in countries in the same region as the city named Yaren
-'Palikir'
-'Agaña'
-'Bairiki'
-'Dalap-Uliga-Darrit'
-'Garapan'
-'Yaren'
-'Koror'
+SELECT c2.Name AS capital
+FROM world.city c2
+JOIN world.country c1 ON c1.Region = (
+    SELECT c1.Region
+    FROM world.city c
+    JOIN world.country c1 ON c.CountryCode = c1.Code
+    WHERE c.Name = 'Yaren'
+)
+WHERE c2.ID = c1.Capital;
 #
 # 19: What unique languages are spoken in the countries in the same region as the city named Riga
-'Belorussian'
-'Estonian'
-'Finnish'
-'Russian'
-'Ukrainian'
-'Lithuanian'
-'Polish'
-'Latvian'
+SELECT DISTINCT cl.Language
+FROM world.countrylanguage cl
+JOIN world.country c1 ON cl.CountryCode = c1.Code
+WHERE c1.Region = (
+    SELECT c2.Region
+    FROM world.city ci
+    JOIN world.country c2 ON ci.CountryCode = c2.Code
+    WHERE ci.Name = 'Riga'
+);
 #
 # 20: Get the name of the most populous city
-Mumbai
+SELECT Name
+FROM world.city
+ORDER BY Population DESC
+LIMIT 1;
+
